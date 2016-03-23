e7d867709d358e12a1132c27ac5f53a8f94aface
sto <Son.C.To@gmail.com>
2016-03-23 15:06:37 -0500

    clojure 1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject  com.kaicode/teleport "0.1.1-SNAPSHOT"
   :source-paths ["src"]
   :dependencies [[com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.237"]])
+                 [com.cognitect/transit-cljs "0.8.237"]
+                 [org.clojure/clojure "1.8.0"]]
+  ;;:main ^:skip-aot com.kaicode.repl
+  )
