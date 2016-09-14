75f9e6fa1aafd64ca84c3abbdb04be4ec65a99ee
sto <Son.C.To@gmail.com>
2016-09-14 14:57:10 -0500

    upgrade transit

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject  com.kaicode/teleport "0.1.1-SNAPSHOT"
   :source-paths ["src"]
-  :dependencies [[com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.237"]
+  :dependencies [[com.cognitect/transit-clj "0.8.288"]
+                 [com.cognitect/transit-cljs "0.8.239"]
                  [org.clojure/clojure "1.8.0"]]
   ;;:main ^:skip-aot com.kaicode.repl
   )
