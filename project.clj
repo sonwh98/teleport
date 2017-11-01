cafe0a866cefa0e2595b81228b6abfe6d29f8bdb
sto <Son.C.To@gmail.com>
2017-11-01 15:51:20 +0200

    rm clojure 1.8.0 dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject  com.kaicode/teleport "0.1.1-SNAPSHOT"
   :source-paths ["src"]
   :dependencies [[com.cognitect/transit-clj "0.8.300"]
-                 [com.cognitect/transit-cljs "0.8.243"]
-                 [org.clojure/clojure "1.8.0"]]
+                 [com.cognitect/transit-cljs "0.8.243"]]
   ;;:main ^:skip-aot com.kaicode.repl
   )
