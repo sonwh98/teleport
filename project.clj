ed7b24e4682dd73ee34f417f642b02ce37357ef0
sto <son.c.to@gmail.com>
2020-02-27 20:55:57 +0700

    upgrade transit-clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject  stigmergy/teleport "0.1.1-SNAPSHOT"
   :source-paths ["src"]
-  :dependencies [[cljscom.cognitect/transit-clj "0.8.319"]
+  :dependencies [[com.cognitect/transit-clj "1.0.324"]
                  [com.cognitect/transit-cljs "0.8.256"]]
   ;;:main ^:skip-aot com.kaicode.repl
   )
